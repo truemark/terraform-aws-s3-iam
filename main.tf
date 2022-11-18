b17c1ae07d4ec243c50e546b9d36b5f84129c216
Erik Jensen <ejensen@truemark.io>
2022-11-18 09:13:35 -0700

    Reformatting code

--- main.tf
+++ main.tf
@@ -3,32 +3,32 @@ data "aws_canonical_user_id" "current" {}
 locals {
   # See https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/AccessLogs.html
   awslogsdelivery_canonical_id = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
-  grant_ids = var.enable_cloudfront_access ? [local.awslogsdelivery_canonical_id, data.aws_canonical_user_id.current.id] : []
-  acl = var.enable_cloudfront_access ? null : var.acl
-  expiration_days = var.expiration_days > 0 ? var.expiration_days : null
-  version_expiration_days = var.version_expiration_days > 0 ? var.version_expiration_days : null
+  grant_ids                    = var.enable_cloudfront_access ? [local.awslogsdelivery_canonical_id, data.aws_canonical_user_id.current.id] : []
+  acl                          = var.enable_cloudfront_access ? null : var.acl
+  expiration_days              = var.expiration_days > 0 ? var.expiration_days : null
+  version_expiration_days      = var.version_expiration_days > 0 ? var.version_expiration_days : null
 }
 
 resource "aws_s3_bucket" "s3" {
-  count = var.create ? 1 : 0
-  bucket = var.name
+  count         = var.create ? 1 : 0
+  bucket        = var.name
   force_destroy = var.force_destroy
-  tags = merge(var.tags, var.s3_tags)
+  tags          = merge(var.tags, var.s3_tags)
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  count = var.create ? 1 : 0
+  count  = var.create ? 1 : 0
   bucket = aws_s3_bucket.s3[count.index].id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = var.kms_master_key_id == null ? "AES256" : "aws:kms"
+      sse_algorithm     = var.kms_master_key_id == null ? "AES256" : "aws:kms"
       kms_master_key_id = var.kms_master_key_id
     }
   }
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "expiration" {
-  count = var.create && local.expiration_days != null ? 1 : 0
+  count  = var.create && local.expiration_days != null ? 1 : 0
   bucket = aws_s3_bucket.s3[count.index].id
   rule {
     id     = "expiration_policy"
@@ -40,7 +40,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "expiration" {
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "version_expiration" {
-  count = var.create && local.version_expiration_days != null ? 1 : 0
+  count  = var.create && local.version_expiration_days != null ? 1 : 0
   bucket = aws_s3_bucket.s3[count.index].id
   rule {
     id     = "version_expiration_policy"
@@ -52,21 +52,21 @@ resource "aws_s3_bucket_lifecycle_configuration" "version_expiration" {
 }
 
 resource "aws_s3_bucket_acl" "acl" {
-  count = var.create && length(local.grant_ids) == 0 ? 1 : 0
+  count  = var.create && length(local.grant_ids) == 0 ? 1 : 0
   bucket = aws_s3_bucket.s3[count.index].id
-  acl = var.acl
+  acl    = var.acl
 }
 
 resource "aws_s3_bucket_acl" "acp" {
-  count = var.create && length(local.grant_ids) > 0 ? 1 : 0
+  count  = var.create && length(local.grant_ids) > 0 ? 1 : 0
   bucket = aws_s3_bucket.s3[count.index].id
   access_control_policy {
     dynamic "grant" {
       for_each = local.grant_ids
       content {
         grantee {
-          type        = "CanonicalUser"
-          id          = grant.value
+          type = "CanonicalUser"
+          id   = grant.value
         }
         permission = "FULL_CONTROL"
       }
@@ -79,7 +79,7 @@ resource "aws_s3_bucket_acl" "acp" {
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
-  count = var.create ? 1 : 0
+  count  = var.create ? 1 : 0
   bucket = aws_s3_bucket.s3[count.index].id
   versioning_configuration {
     status = var.versioning ? "Enabled" : "Disabled"
@@ -87,20 +87,20 @@ resource "aws_s3_bucket_versioning" "versioning" {
 }
 
 resource "aws_s3_bucket_public_access_block" "s3" {
-  count = var.create ? 1 : 0
-  bucket = aws_s3_bucket.s3[count.index].id
-  block_public_acls = var.block_public_acls
-  block_public_policy = var.block_public_policy
+  count                   = var.create ? 1 : 0
+  bucket                  = aws_s3_bucket.s3[count.index].id
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
   restrict_public_buckets = var.restrict_public_buckets
-  ignore_public_acls = var.ignore_public_acls
+  ignore_public_acls      = var.ignore_public_acls
 }
 
 resource "aws_iam_policy" "rw" {
-  count = var.create ? 1 : 0
-  name = "${var.name}-read-write"
-  path = var.path
+  count       = var.create ? 1 : 0
+  name        = "${var.name}-read-write"
+  path        = var.path
   description = "Read-write access to S3 bucket ${var.name}"
-  policy = <<EOF
+  policy      = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -133,11 +133,11 @@ EOF
 }
 
 resource "aws_iam_policy" "ro" {
-  count = var.create ? 1 : 0
-  name = "${var.name}-read-only"
-  path = var.path
+  count       = var.create ? 1 : 0
+  name        = "${var.name}-read-only"
+  path        = var.path
   description = "Read-only access to S3 bucket ${var.name}"
-  policy = <<EOF
+  policy      = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -168,31 +168,31 @@ EOF
 
 resource "aws_iam_user" "rw" {
   count = var.create && var.create_rw_user ? 1 : 0
-  name = var.rw_user_name == null ? "${var.name}-rw" : var.rw_user_name
-  path = var.path
-  tags = merge(var.tags, var.rw_user_tags)
+  name  = var.rw_user_name == null ? "${var.name}-rw" : var.rw_user_name
+  path  = var.path
+  tags  = merge(var.tags, var.rw_user_tags)
 }
 
 resource "aws_iam_user_policy_attachment" "rw" {
-  count = var.create && var.create_rw_user ? 1 : 0
+  count      = var.create && var.create_rw_user ? 1 : 0
   policy_arn = aws_iam_policy.rw[count.index].arn
-  user = aws_iam_user.rw[count.index].name
+  user       = aws_iam_user.rw[count.index].name
 }
 
 resource "aws_iam_user" "ro" {
   count = var.create && var.create_ro_user ? 1 : 0
-  name = var.ro_user_name == null ? "${var.name}-ro" : var.ro_user_name
-  path = var.path
-  tags = merge(var.tags, var.ro_user_tags)
+  name  = var.ro_user_name == null ? "${var.name}-ro" : var.ro_user_name
+  path  = var.path
+  tags  = merge(var.tags, var.ro_user_tags)
 }
 
 resource "aws_iam_user_policy_attachment" "ro" {
-  count = var.create && var.create_ro_user ? 1 : 0
+  count      = var.create && var.create_ro_user ? 1 : 0
   policy_arn = aws_iam_policy.ro[count.index].arn
-  user = aws_iam_user.ro[count.index].name
+  user       = aws_iam_user.ro[count.index].name
 }
 
-data aws_iam_policy_document "bucket_notification_topic_policy" {
+data "aws_iam_policy_document" "bucket_notification_topic_policy" {
   statement {
     effect = "Allow"
     principals {
@@ -212,13 +212,13 @@ data aws_iam_policy_document "bucket_notification_topic_policy" {
 }
 
 resource "aws_sns_topic" "bucket_notification_topic" {
-  count = var.create && var.create_bucket_notification_topic ? 1 : 0
-  name = "${var.name}-notifications"
+  count  = var.create && var.create_bucket_notification_topic ? 1 : 0
+  name   = "${var.name}-notifications"
   policy = data.aws_iam_policy_document.bucket_notification_topic_policy.json
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
-  count = var.create && var.create_bucket_notification_topic ? 1 : 0
+  count  = var.create && var.create_bucket_notification_topic ? 1 : 0
   bucket = aws_s3_bucket.s3[count.index].id
   topic {
     events    = ["s3:ObjectCreated:*"]
--- variables.tf
+++ variables.tf
@@ -1,91 +1,91 @@
 variable "create" {
   description = "Determines if resources are created."
-  default = true
-  type = bool
+  default     = true
+  type        = bool
 }
 
 variable "name" {
   description = "Name to use on created resources"
-  type = string
+  type        = string
 }
 
 variable "path" {
   description = "Path to use when creating IAM policies and IAM users"
-  default = "/"
-  type = string
+  default     = "/"
+  type        = string
 }
 
 variable "acl" {
   description = "Canned ACL applied to the bucket. Conflicts with enable_cloudfront_access."
-  default = "private"
-  type = string
+  default     = "private"
+  type        = string
 }
 
 variable "versioning" {
   default = false
-  type = bool
+  type    = bool
 }
 
 variable "force_destroy" {
   default = false
-  type = bool
+  type    = bool
 }
 
 variable "block_public_acls" {
   default = true
-  type = bool
+  type    = bool
 }
 
 variable "block_public_policy" {
   default = true
-  type = bool
+  type    = bool
 }
 
 variable "restrict_public_buckets" {
   default = true
-  type = bool
+  type    = bool
 }
 
 variable "ignore_public_acls" {
   default = true
-  type = bool
+  type    = bool
 }
 
 variable "create_ro_user" {
   default = false
-  type = bool
+  type    = bool
 }
 
 variable "ro_user_name" {
   description = "Name of the read-only user. Default is {var.name}-ro."
-  default = null
-  type = string
+  default     = null
+  type        = string
 }
 
 variable "create_rw_user" {
   default = false
-  type = bool
+  type    = bool
 }
 
 variable "rw_user_name" {
   description = "Name of the read-write user. Default is {var.name}-rw."
-  default = null
-  type = string
+  default     = null
+  type        = string
 }
 
 variable "enable_cloudfront_access" {
   default = false
-  type = bool
+  type    = bool
 }
 
 variable "expiration_days" {
   default = -1
-  type = number
+  type    = number
 }
 
 variable "version_expiration_days" {
   default = -1
-  type = number
+  type    = number
 }
 
 variable "tags" {
@@ -97,29 +97,29 @@ variable "tags" {
 variable "s3_tags" {
   description = "A map of tags to add to the s3 bucket."
   type        = map(string)
-  default = {}
+  default     = {}
 }
 
 variable "rw_user_tags" {
   description = "A map of tags to add to the read-write IAM user."
   type        = map(string)
-  default = {}
+  default     = {}
 }
 
 variable "ro_user_tags" {
   description = "A map of tags to add to the read-only IAM user."
   type        = map(string)
-  default = {}
+  default     = {}
 }
 
 variable "kms_master_key_id" {
   description = "Optional AWS KMS master key ID to use for SSE-KMS encryption"
-  type = string
-  default = null
+  type        = string
+  default     = null
 }
 
 variable "create_bucket_notification_topic" {
   description = "Creates a SNS topic to receive bucket notifications"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
